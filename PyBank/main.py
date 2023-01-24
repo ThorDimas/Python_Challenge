d99caf8fdb97447ab02b3a2f084e11248e59e7da
Thoribio <119829678+ThorDimas@users.noreply.github.com>
2023-01-23 23:14:07 -0600

    Finalized pybank challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,46 +9,54 @@ prev_pnl = 0
 month_of_change = []
 pnl_change_list = []
 great_incr_profit = ["", 0]
-great_decr_prof = ["", 99999999999999999]
+great_decr_prof = ["", 9999999999]
 profit_losses = 0
 
 
 with open(PyBank_path) as bankfile:
     reader = csv.DictReader(bankfile)
-
-    for row in reader:
-        
-        #track the totals
+    #Dictreader instead of reader to avoid using next for the first row. Use the name of the header
+    for row in reader:        
         months = months + 1
         profit_losses = profit_losses + int(row['Profit/Losses'])
 
-        #track the revenue change
+        #append and calculate the revenue change over each iteration of row
         change_in_pnl = int(row['Profit/Losses']) - prev_pnl
         prev_pnl = int(row['Profit/Losses'])
         pnl_change_list = pnl_change_list + [change_in_pnl]
         month_of_change = month_of_change + [row['Date']]
 
-        #Calculate th greates increase
+        #Calculate th greates increase. If the change in pnl is higher, then append the month and amount
         if change_in_pnl > great_incr_profit[1]:
             great_incr_profit[0] = row['Date']
             great_incr_profit[1] = change_in_pnl
 
-        #calculate the greates decrease
+        #same for decrease
         if change_in_pnl < great_decr_prof[1]:
             great_decr_prof[0] = row['Date']
             great_decr_prof[1] = change_in_pnl
 
 #calculate the average revenue change
-pnl_average = sum(pnl_change_list) /len(pnl_change_list)
+#Note that I tryed different approaches and still not sure why I'm getting a different result in the average change
 
-#print the results
-print(
+pnl_change_list.pop(0)
+total_change = sum(pnl_change_list)
+total_months = months
+pnl_average = total_change/total_months
+
+# #print the results
+results = (
 f'Financial Analysis\n'
 f'--------------------------\n'
 f'Total Months: {months}\n'
 f'Total: ${profit_losses}\n'
 f'Average Change: ${pnl_average}\n'
-f'Greatest Increase in Profits: {great_incr_profit}'
-f'Greatest Decrease in Profits: {great_decr_prof}'    
+f'Greatest Increase in Profits: {great_incr_profit[0]} (${great_incr_profit[1]})\n'
+f'Greatest Decrease in Profits: {great_decr_prof[0]} (${great_decr_prof[1]})'    
 )
+print(results)
 
+pybank_results = os.path.join('analysis', 'analysis.txt')
+
+with open(pybank_results, "w") as results_file:
+    results_file.write(results)
